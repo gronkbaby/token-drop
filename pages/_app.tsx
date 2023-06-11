ee2b8daddf7499c4548eed6e035b36a2b4966298
gronkbaby <136215593+gronkbaby@users.noreply.github.com>
2023-06-11 13:47:43 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import { AppProps } from "next/app";
 import { Web3Provider } from "@ethersproject/providers";
+import { ethers } from "ethers";
 import { ChainId } from "@uniswap/sdk";
 
 const activeChainId = ChainId.MAINNET; // Set the Ethereum chain ID here
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Web3Provider>
+    <Web3Provider
+      library={new ethers.providers.Web3Provider(window.ethereum)}
+      options={{
+        chainId: activeChainId,
+      }}
+    >
       <Component {...pageProps} />
     </Web3Provider>
   );
 }
 
 export default MyApp;
+
