1bcbb227b084b86334ad85196d45b804f58d71c3
gronkbaby <136215593+gronkbaby@users.noreply.github.com>
2023-06-11 12:13:46 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,12 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mumbai;
+const activeChainId = ChainId.Ethereum;
+const tokenAddress = "0x8494E2E992a0669B86174d79B98750F7827bA4F3";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider desiredChainId={activeChainId} tokenAddress={tokenAddress}>
       <Head>
         <title>thirdweb Token Drop Example</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
