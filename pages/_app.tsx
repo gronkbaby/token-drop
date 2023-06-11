bde3ce5368745b1731ecac8c89e2e93ad654155f
gronkbaby <136215593+gronkbaby@users.noreply.github.com>
2023-06-11 13:45:59 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,11 @@ import { Web3Provider } from "@ethersproject/providers";
 import { ChainId } from "@uniswap/sdk";
 
 const activeChainId = ChainId.MAINNET; // Set the Ethereum chain ID here
-const tokenAddress = "0x8494E2E992a0669B86174d79B98750F7827bA4F3";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Web3Provider chainId={activeChainId}>
-      <Component {...pageProps} tokenAddress={tokenAddress} />
+    <Web3Provider>
+      <Component {...pageProps} />
     </Web3Provider>
   );
 }
